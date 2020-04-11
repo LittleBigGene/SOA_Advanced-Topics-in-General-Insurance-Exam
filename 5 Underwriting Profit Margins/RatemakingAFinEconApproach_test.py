5fc9b957852bac3ecec0a3ab32af4d8e2d4df017
Jin Cai <Gene@MP09.local>
2020-04-11 15:55:19 -0700

    spring_16_5

--- 5 Underwriting Profit Margins/RatemakingAFinEconApproach_test.py
+++ 5 Underwriting Profit Margins/RatemakingAFinEconApproach_test.py
@@ -41,5 +41,32 @@ class test_RatemakingAFinEconApproach(unittest.TestCase):
         # The model only covers risk that varies with market returns. As such, it ignores unique insurance risks such as catastrophe.
         # The insurance market cannot simply be appended to the stock market.
         
+
+    def test_spring_16_5(self):
+
+        pvL = 70 / 2 / 0.94 + 70 / 2 / 0.94 / 0.94
+        pvE = 20
+
+        p = 99.7
+
+        pvTUW = (p-20) * 0.4 / 1.01 - 70 * 0.4 / 0.94
+        pvTII = (p+100-20) * 0.01 * .4 /1.01 + (p + 50 - 20 - 35) * 0.01 * 0.4 / 1.01**2
+
+        #a       
+        self.assertAlmostEqual(p, pvL + pvE + pvTUW + pvTII , 2)
+
+        #b) You are also considering using the Target Total Rate of Return Model. 
+        #   You need to decide whether to use statutory surplus or the company’s actual equity to derive the required underwriting profit margin.
+
+        # Actual equity should be used, because using statutory surplus has the following issues:
+        #  It is usually lower than actual equity and thus will lead to a lower profit margin, which may lead to investment in risky assets.
+        #  It ignores the time value of money.
+        #  It excludes tangible assets and some reinsurance.
+        #  It does not value bonds and real estate at market value.
+
+        #c) Explain the adjustment to the Capital Asset Pricing Model (CAPM) needed to reflect increased risk.
+        
+        # The absolute value of beta should be increased.
+
 if __name__ == '__main__':
     unittest.main()
