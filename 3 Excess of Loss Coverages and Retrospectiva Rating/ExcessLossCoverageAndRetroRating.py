0438c295465bdb5e5fefa78c3f5f44b4108cda12
Jin Cai <Gene@MP09.local>
2020-04-11 16:49:28 -0700

    spring_16_6

--- 3 Excess of Loss Coverages and Retrospectiva Rating/ExcessLossCoverageAndRetroRating.py
+++ 3 Excess of Loss Coverages and Retrospectiva Rating/ExcessLossCoverageAndRetroRating.py
@@ -2,16 +2,14 @@
 # Lee
 
 class Excess_Loss_Coverage_And_RetroRating:
-    Layers = []
-    ExcessAmounts = []
-    Trend = 0
-    TrendFactors = []
 
-    def __init__(self, layers, excessAmts, trend):
-        self.Layers = layers
-        self.ExcessAmounts = excessAmts
-        self.Trend = trend
-    
+    def __init__(self):
+        self.Layers = []
+        self.ExcessAmounts = []
+        self.Trend = 0
+        self.TrendFactors = []
+        self.Table_M_Loss_Ratios = []
+
     def set_losses_probabilities(self, losses, probabilities):        
         self.TrendFactors = []
         for layer, excess in zip(self.Layers, self.ExcessAmounts):                       
@@ -20,5 +18,31 @@ class Excess_Loss_Coverage_And_RetroRating:
             for loss, prob in zip(losses, probabilities):
                 layerExcessLoss += prob * min(max(loss - layer, 0), excess)    
                 trendedLayerExcessLoss += prob * min(max(loss * (1 + self.Trend) - layer,0), excess)    
-            print(f'Trend Factor {trendedLayerExcessLoss / layerExcessLoss}')
+            #print(f'Trend Factor {trendedLayerExcessLoss / layerExcessLoss}')
             self.TrendFactors.append(trendedLayerExcessLoss / layerExcessLoss)    
+
+    # The entry ratio: The multiple of a risk’s expected loss or expected loss ratio
+
+    # Table M charge: The average amount by which a risk’s actual loss exceeds r times its expected loss, divided by its expected loss
+    def Φ(self, r):
+        avg_loss_ratio = sum(self.Table_M_Loss_Ratios) / len(self.Table_M_Loss_Ratios)
+        expected_excess = r * avg_loss_ratio
+
+        excess = 0
+        for loss in self.Table_M_Loss_Ratios:
+            if loss > expected_excess:
+                excess += loss - expected_excess
+            
+        return excess / len(self.Table_M_Loss_Ratios) / avg_loss_ratio
+
+    # Table M saving: The average amount by which a risk’s actual loss falls short of r times its expected loss, divided by its expected loss
+    def Ψ(self, r):
+        avg_loss_ratio = sum(self.Table_M_Loss_Ratios) / len(self.Table_M_Loss_Ratios)
+        expected_excess = r * avg_loss_ratio
+
+        saving = 0
+        for loss in self.Table_M_Loss_Ratios:
+            if loss < expected_excess:
+                saving += expected_excess - loss
+            
+        return saving / len(self.Table_M_Loss_Ratios) / avg_loss_ratio
--- 3 Excess of Loss Coverages and Retrospectiva Rating/ExcessLossCoverageAndRetroRating_test.py
+++ 3 Excess of Loss Coverages and Retrospectiva Rating/ExcessLossCoverageAndRetroRating_test.py
@@ -3,7 +3,10 @@ from ExcessLossCoverageAndRetroRating import Excess_Loss_Coverage_And_RetroRatin
 
 class test_ExcessLossCoverageAndRetroRating(unittest.TestCase):
     def test_spring_19_6(self):
-        coverage = Excess_Loss_Coverage_And_RetroRating([0,100,200], [100,100,999], 0.1)
+        coverage = Excess_Loss_Coverage_And_RetroRating()
+        coverage.Layers = [0,100,200]
+        coverage.ExcessAmounts = [100,100,999]
+        coverage.Trend = 0.1
         
         coverage.set_losses_probabilities([150, 250], [0.5,0.5])        
         self.assertLess(coverage.TrendFactors[0],coverage.TrendFactors[1])
@@ -20,5 +23,41 @@ class test_ExcessLossCoverageAndRetroRating(unittest.TestCase):
     def test_fall_16_3(self):
         pass
 
+    def test_spring_16_6(self):
+        retro = Excess_Loss_Coverage_And_RetroRating()
+
+        #a) define Table M in a retro rating plan
+
+        #b
+        retro.Table_M_Loss_Ratios = [.3,.4,.5,.6,.7]
+
+        r = 0
+        self.assertAlmostEqual(1,retro.Φ(r))
+        self.assertAlmostEqual(0,retro.Ψ(r))
+
+        r = 0.4
+        self.assertAlmostEqual(0.6, retro.Φ(r))
+        self.assertAlmostEqual(0 , retro.Ψ(r))
+        
+        r = 0.8
+        self.assertAlmostEqual(0.24, retro.Φ(r))
+        self.assertAlmostEqual(0.04, retro.Ψ(r))
+        
+        r = 1.2 
+        self.assertAlmostEqual(0.04, retro.Φ(r))
+        self.assertAlmostEqual(0.24, retro.Ψ(r))
+
+        r = 1.6
+        self.assertAlmostEqual( 0 , retro.Φ(r))
+        self.assertAlmostEqual(0.6 , retro.Ψ(r))
+
+        r = 2
+        self.assertAlmostEqual( 0 ,retro.Φ(r))
+        self.assertAlmostEqual( 1 , retro.Ψ(r))
+
+        #c) Explain how Table L differs from Table M.
+        # Table L incorporates a per accident limit on losses.
+
+        
 if __name__ == '__main__':
     unittest.main()
