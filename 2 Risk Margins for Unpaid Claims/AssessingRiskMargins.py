63860d25d18fcf01bf167ddc05140fd7960c0a09
Jin Cai <Gene@MP09.local>
2020-04-12 16:16:58 -0700

    fall_17_2

--- 2 Risk Margins for Unpaid Claims/AssessingRiskMargins.py
+++ 2 Risk Margins for Unpaid Claims/AssessingRiskMargins.py
@@ -1,9 +1,28 @@
 # CAS Exam 7
 # Marshall et al.
-
+import numpy as np
 class AssessingRiskMargins():
 
-    pass
+    def risk_margin(self, coefficient_of_variation, claim_liabilities, z_75th = 0.674 ):
+        return coefficient_of_variation * claim_liabilities * z_75th
+
+    def combined_coefficient_of_variation(self, sources):
+        combined = 0
+        for source in sources:
+            combined += source ** 2
+        
+        return combined ** 0.5
+
+    def risk_coefficient_of_variation(self, risks, claim_liabilities, corr = 0):
+        combined = 0
+        total_liab = sum(claim_liabilities)
+        for risk, liab in zip(risks, claim_liabilities):
+            combined += (risk * liab / total_liab) ** 2
+        
+        # covariance
+        combined += 2 * corr * np.prod(risks) * np.prod(claim_liabilities) / (total_liab ** 2)
+        
+        return combined ** .5
 
     # two areas of additional analysis that you may conduct to provide further comfort regarding the outcomes from the deployment of this framework.
                 
@@ -13,7 +32,6 @@ class AssessingRiskMargins():
         #  External benchmarking: Review differences between benchmarks and the claim portfolio being analyzed.
         #  Hindsight analysis: Compare past estimates against the latest view of the equivalent liabilities.
 
-
     #four approaches that can be used to analyze independent sources of risk.
                 
         #  Mack method
@@ -21,3 +39,11 @@ class AssessingRiskMargins():
         #  Stochastic chain ladder
         #  Generalized linear models
         #  Bayesian
+
+    # Parameter selection error for internal systemic risk
+    #   The parameter selection error for internal systemic risk is the error from the model 
+    #   being unable to adequately measure all predictors of claim cost outcomes or trends.
+    
+    # Random component of parameter risk
+    #   The random component of parameter risk is the extent to which the randomness associated 
+    #   with the insurance process compromises the ability to select appropriate parameters.
--- 2 Risk Margins for Unpaid Claims/AssessingRiskMargins_test.py
+++ 2 Risk Margins for Unpaid Claims/AssessingRiskMargins_test.py
@@ -7,18 +7,16 @@ class test_AssessingRiskMargins(unittest.TestCase):
         pass
 
     def test_spring_16_7(self):
-        uncertainties = [0.06, 0.08, 0.16]
+        arm = AssessingRiskMargins()
 
-        #a        
-        combined = 0
-        for source in uncertainties:
-            combined += source ** 2
+        sources = [0.06, 0.08, 0.16]
         
-        combined_coefficient = combined ** 0.5
+        #a        
+        combined_coefficient = arm.combined_coefficient_of_variation(sources)
         self.assertAlmostEqual(.1887, combined_coefficient, 4)
 
         #b
-        riskMargin = combined_coefficient * 216 * 10**6 * .674
+        riskMargin = arm.risk_margin(combined_coefficient, 216 * 10**6 )
         self.assertAlmostEqual(27468734, riskMargin, 0)
 
         #c) Describe two areas of additional analysis that you may conduct to provide further comfort regarding the outcomes from the deployment of this framework.
@@ -36,6 +34,39 @@ class test_AssessingRiskMargins(unittest.TestCase):
         #  Information may not be credible
         #  May be costly to work at the granular level
         #  Extra detail may not provide material improvement
+
+    def test_fall_17_2(self):
+        #a) Describe each of the following sources of risk:
+        # Parameter selection error for internal systemic risk
+        # Random component of parameter risk
+
+        #b
+        arm = AssessingRiskMargins()       
+        claim_liabilities = [7500,4000]
+
+        independent_risk = [0.07, 0.1]
+        ind_risk_coeff = arm.risk_coefficient_of_variation(independent_risk, claim_liabilities)        
+        self.assertAlmostEqual(0.057, ind_risk_coeff, 3)
+
+        internal_systemic_risk = [.06, .09]
+        internal_systemic_risk_coeff = arm.risk_coefficient_of_variation(internal_systemic_risk, claim_liabilities, 0.25)        
+        self.assertAlmostEqual(0.056, internal_systemic_risk_coeff, 3)
+
+        aggregate = arm.combined_coefficient_of_variation([ind_risk_coeff,internal_systemic_risk_coeff,0.032])
+
+        rm = arm.risk_margin(aggregate, sum(claim_liabilities))
+        self.assertAlmostEqual(667+2, rm, 0)
+
+        #c) Your manager also asked you to provide an estimate of the risk margin at the 99.5% adequacy level on the same portfolio.
+        #   Explain why using the approach of part (b) may not be appropriate.
+
+        #   Any two of the following were sufficient for full credit.
+        #   • The normal distribution may not be appropriate for an extreme percentile.
+        #   • The internal systemic risk correlation in the extreme tails may be more than 25%.
+        #   • There may be other risk drivers than those used.
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
